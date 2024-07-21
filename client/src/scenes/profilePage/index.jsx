03fd6c841dc5bfd672e0412a11dfa135277cd41a
jonathen <jonathencheng86@gmail.com>
2024-07-21 16:30:31 +0800

    added profile page

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+//import React from 'react'
+import {Box, useMediaQuery} from "@mui/material"
+import {useEffect, useState} from "react"
+import { useSelector } from 'react-redux'
+import {useParams} from 'react-router-dom'
+import Navbar from 'scenes/navbar'
+import MyPostWidget from 'scenes/widgets/MyPostWidget'
+import PostsWidget from 'scenes/widgets/PostsWidget'
+import UserWidget from 'scenes/widgets/UserWidget'
 
 const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
-}
+  const [user, setUser] = useState(null);
+  const {userId} = useParams(); // grabing userId from URL
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
-export default ProfilePage
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET", 
+      headers: {Authorization: `Bearer ${token}`},
+    });
+
+    const data = await response.json();
+    setUser(data);
+  }
+
+  useEffect(() => {
+    getUser();
+  }, [])// eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
+
+  return (
+    <Box>
+       <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+        alignItems={isNonMobileScreens ? "flex-start" : "center"} // Ensure alignment
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={userId} userPicturePath={user.userPicturePath} />
+        </Box>  
+        <Box
+          flexBasis={isNonMobileScreens ? "50%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >          
+          {<PostsWidget userId={userId} isProfile />
+          }
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProfilePage;
